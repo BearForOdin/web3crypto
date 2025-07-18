897b63fe54a2da381246a2d49220ac834ee52a3e
BearForOdin <selasidzeamesi82@gmail.com>
2025-07-18 09:27:12 -0700

    Solve-conflict

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+import { hiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../images/logo.png';
+
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <nav className="w-fill flex md:justify-center">
+            
+        </nav>
     );
 }
 
